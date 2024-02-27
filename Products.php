56c7ba8fb89dcbf05c42e165fc0058db5967a00a
Adam Ahmed Virk <adam.a.v@outlook.com>
2024-02-27 11:41:10 +0100

    modified:   Products.php

--- Products.php
+++ Products.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $servername = "localhost";
 $username = "root";
@@ -31,3 +39,6 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
+   
+</body>
+</html>
