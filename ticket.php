45ab40cb74b4a9174cc18ea4eb9820f0e2a090c9
Adam Ahmed Virk <adam.a.v@outlook.com>
2024-03-12 13:52:35 +0100

    modified:   style.css 	modified:   ticket.php

--- ticket.php
+++ ticket.php
@@ -31,7 +31,46 @@
     <button id="dark" onclick="dark()">Dark</button>
 
     <button id="redirectButton" >Login inn</button>
-        
+    <div class="Ticket">
+        <form method="post">
+            <div class="row">
+                <div class="col-25">
+                    <label for="fname">First Name</label>
+                </div>
+                <div class="col-75">
+                    <input type="text"  name="firstname" placeholder="Your name..">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="lname">Last Name</label>
+                </div>
+                <div class="col-75">
+                    <input type="text"  name="lastname" placeholder="Your last name..">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="Email">Email</label>
+                </div>
+                <div class="col-75">
+                    <input type="text" name="Email" placeholder="Email">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="subject">Subject</label>
+                </div>
+                <div class="col-75">
+                    <textarea  name="subject" placeholder="Write the problem"
+                        style="height:200px"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
         <script src="script.js" async defer></script>
     </body>
 </html>
